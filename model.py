cd8af5dc7007f3c2e2b92a5e87561c6ba3fdae9d
dsl <dsl>
2018-11-23 10:43:09 +0800

    n

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import math
 import os
-NUM_TIMESTEPS = 60
+NUM_TIMESTEPS = 90
 
 
 data = np.load('data.npy')
@@ -13,8 +13,10 @@ data = data.reshape(-1, 3)
 scaler = MinMaxScaler(feature_range=(0, 1), copy=False)
 data = scaler.fit_transform(data)
 print(data.shape)
-X = np.zeros((data.shape[0], NUM_TIMESTEPS, data.shape[1]))
-Y = np.zeros((data.shape[0],data.shape[1]))
+X = np.zeros((data.shape[0]-NUM_TIMESTEPS, NUM_TIMESTEPS, data.shape[1]))
+Y = np.zeros((data.shape[0]-NUM_TIMESTEPS,data.shape[1]))
+
+
 
 for i in range(len(data) - NUM_TIMESTEPS - 1):
     X[i] = data[i:i + NUM_TIMESTEPS]
@@ -23,12 +25,12 @@ for i in range(len(data) - NUM_TIMESTEPS - 1):
 
 #X = np.expand_dims(X, axis=2)
 
-sp = int(0.8 * len(data))
+sp = int(0.9 * X.shape[0])
 Xtrain, Xtest, Ytrain, Ytest = X[0:sp], X[sp:], Y[0:sp], Y[sp:]
 
 print(Xtrain.shape, Xtest.shape, Ytrain.shape, Ytest.shape)
 
-HIDDEN_SIZE = 10
+HIDDEN_SIZE = 20
 BATCH_SIZE = 1
 
 
@@ -39,8 +41,18 @@ model.add(LSTM(HIDDEN_SIZE, stateful=True,
 model.add(Dense(3))
 model.compile(loss="mean_squared_error", optimizer="adam",
     metrics=["mean_squared_error"])
-
+model.summary()
 
 model.fit(Xtrain, Ytrain, epochs=10, batch_size=BATCH_SIZE,
     validation_data=(Xtest, Ytest),
     shuffle=False)
+
+x_test = data[-NUM_TIMESTEPS:]
+result = []
+for x in range(15):
+    x_data = np.expand_dims(x_test[-NUM_TIMESTEPS:],0)
+    resul = model.predict(x_data)
+    print(resul)
+    print(x_test[-15:])
+    x_test = np.vstack((x_test, resul))
+
